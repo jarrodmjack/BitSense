97796e4ee16e229f5b618ec7e35ed8f5c1ff5c58
jarrodmjack <jarrodmjack@gmail.com>
2023-07-10 12:15:29 -0400

    adjust login button

--- frontend/src/components/button/LoginButton.tsx
+++ frontend/src/components/button/LoginButton.tsx
@@ -12,7 +12,7 @@ const LoginButton = () => {
 		<button
         onClick={handleRedirectToLogin}
 			type="button"
-			className="text-white bg-violet-800 hover:bg-violet-950 focus:ring-4 focus:outline-none font-medium rounded-lg text-sm px-4 py-2 text-center mr-3 md:mr-0"
+			className="text-violet-800 hover:bg-violet-950 focus:ring-4 focus:outline-none font-medium rounded-lg text-sm px-4 py-2 text-center mr-3 md:mr-0"
 		>
 			Log in
 		</button>
